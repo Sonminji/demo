50af6a63ae49ecd1a5cd84df4f78ba7cb0f4526e
dsmj <dsmj60@disec.kr>
2023-01-27 17:51:43 +0900

    홈페이지 구성

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,35 +2,55 @@ package com.example.demo.controller;
 
 import com.example.demo.model.UserInfo;
 import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    //회원가입
     @GetMapping("/join")
-    public String create(UserInfo userInfo){
-        userInfo.setId("aa");
-        userInfo.setPassword("bbb111");
-        userInfo.setName("name!");
+    public String join(Model model){
+        UserInfo userInfo = new UserInfo();
+        model.addAttribute("UserInfo",userInfo);
+        return "/config/user/join.html";
+    }
+
+
+    //회원가입
+    @PostMapping(value = "/joinUser")
+    public String joinUser(@RequestBody UserInfo userInfo) throws Exception {
+
+        Object obj = userInfo;
+        for(Field field : obj.getClass().getDeclaredFields()){
+            field.setAccessible(true);
+            Object value = field.get(obj);
+            System.out.println("field : "+field.getName()+" | value : " + value);
+        }
+
         return userService.save(userInfo);
     }
 
-    //모든사용자검색
+    //모든 사용자 검색
     @GetMapping("/findAllUser")
+    @ResponseBody
     public Collection<UserInfo> getInfo(){
         userService.findAll().forEach(user -> System.out.println(user));
         return userService.findAll();
     }
 
+    // 특정 사용자 검색
     @GetMapping("/search/{id}")
     public Optional<UserInfo> search(@PathVariable String id){
         return userService.findByUserId(id);
--- src/main/java/com/example/demo/model/UserInfo.java
+++ src/main/java/com/example/demo/model/UserInfo.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 
 
 @Data
@@ -22,12 +24,41 @@ public class UserInfo {
     @Column(name = "password")
     private String password;
 
-    @Builder
+    @NotNull
+    @Column(name = "phone_number")
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
+    private String phoneNumber;
+    @Column(name = "home_number")
+    private String homeNumber;
+    @NotNull
+    @Column(name = "email")
+    @Pattern(regexp = "[a-zA-z0-9]+@[a-zA-z]+[.]+[a-zA-z.]+", message = "이메일 형식에 맞게 작성해주세요.")
+    private String email;
+
+    @Column(name = "birthdate")
+    private String birthdate;
+
+    @Column(name = "address")
+    private String address;
+
     public UserInfo(String id, String name, String password){
         this.id = id;
         this.name = name;
         this.password = password;
     }
+
+    @Builder
+    public UserInfo(String id, String name, String password, String phoneNumber, String homeNumber, String email, String birthdate, String address) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.homeNumber = homeNumber;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.address = address;
+    }
+
 
     public UserInfo() {
 
